d819472d7d08fdbb11c385d7271890fa70f2a03b
IronJarl <154372513+IronJarl@users.noreply.github.com>
2026-03-26 16:34:49 -0700

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,29 +1,23 @@
 #Time is absolutely required to not fry the device
 import time
 #No use checking for a prime number less than 2
-y=3
+y=1
 prime=[2]
 try:
     while True:
         time.sleep(0.1)
+        y +=2
         z=0
-        #K for key, how clever
-        k=0
-        #Applying the theorem of "composite number must have a prime factor no greater than it's square root"
-        while prime[z]**2<=y:  
-            #Basic modular division to check for remainder, it's much easier for a computer this way"      
-            if y%prime[z]==0:
-                k=1
+        while True:  
+             #Basic modular division to check for remainder, it's much easier for a computer this way"      
+            if z==len(prime)-1 or prime[z]**2>y: #Applying theorem of composite numbers and a factor less than or equal to it's root
+                #Got rid of my key idea, it was slow
+                prime.append(y)
                 break
-            elif z==len(prime)-1:
+            elif y%prime[z] == 0:
                 break
             else:
                 z +=1
-        #The door to primehood
-        if k==0:
-            prime.append(y)
-        #Count by twos since all even numbers are naturally composite
-        y +=2
 #Escape method once you have let it run long enough
 except KeyboardInterrupt:
     #List and other important bits of info
