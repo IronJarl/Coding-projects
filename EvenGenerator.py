b5ba7ee615426411c039cdfbf9063b24832ecd17
IronJarl <154372513+IronJarl@users.noreply.github.com>
2026-03-29 21:00:38 -0700

    Update EvenGenerator.py

--- EvenGenerator.py
+++ EvenGenerator.py
@@ -1,13 +1,17 @@
-
+#Cannot forget the time, don't want to burn out the computer
 import time
 def Even():
+     #0 is in fact an even number despite it's appearance
      num=0
      while True:
             time.sleep(.1)
+            #I believe this is the basis of all generators so it's needed
             yield num
             num +=2
 try:
     for value in Even():
         print(value)
+#Can't forget about the escape route
 except KeyboardInterrupt:
     print("That's enough")
+#For some reason, my comments weren't visible in the repo. Updating it
